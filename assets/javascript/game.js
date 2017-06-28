d4ea2a0771df8201d971a6455efe3b7e96b5b261
a-bhuyan <arnob.bhuyan@gmail.com>
2017-06-27 23:13:43 -0500

    v1

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+
+
+//$("#crystal1", "#crystal2", "#crystal3", "#crystal4").empty();
+
+$(document).ready(function() {
+	
+var targetNum,
+	crystal1,
+	crystal2,
+	crystal3,
+	crystal4,
+	winCounter,
+	lossCounter;
+
+function initialize(){
+	targetNum = "";
+	crystal1 = "";
+	crystal2 = "";
+	crystal3 = "";
+	crystal4 = "blah";
+}
+
+
+
+	intialize();
+	console.log(crystal4);
+
+
+
+
+
+});
